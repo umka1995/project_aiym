1f59f57fe50527e090d0825f116343f7dfd3fffb
umka1995 <umka.key@gmail.com>
2023-06-13 21:10:22 +0600

    x

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
-    path('activate/', ActivationView.as_view()),
+    path('activate/<str:email>/<str:activation_code>/', ActivationView.as_view(), name='activate'),
+    # path('activate/', ActivationView.as_view()),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('change_password/', ChangePasswordView.as_view()),
--- account/utils.py
+++ account/utils.py
@@ -1,29 +1,29 @@
 from django.core.mail import send_mail
-# from django.template.loader import render_to_string
-# from django.utils.html import strip_tags
-
-# def send_activation_code(email, activation_code):
-#     context = {
-#         'text_detail': 'Спасибо за регистрацию',
-#         'email': email,
-#         'domain': 'http://localhost:8000',
-#         'activation_code': activation_code
-
-#     }
-#     msg_html= render_to_string('index.html',context)
-#     message = strip_tags(msg_html)
-#     send_mail('Account activation', message,'admin@gmail.com',[email], html_message=msg_html, fail_silently=False)
-
-
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 def send_activation_code(email, activation_code):
-    message = f'Спасибо за регистрацию, Мы вам отправили\n . Код активации: {activation_code}'
-    send_mail(
-        'Активация аккаунта',
-        message,
-        'test@gmail.com',
-        [email]
-    )
+    context = {
+        'text_detail': 'Спасибо за регистрацию',
+        'email': email,
+        'domain': 'http://34.125.224.175',
+        'activation_code': activation_code
+
+    }
+    msg_html= render_to_string('index.html',context)
+    message = strip_tags(msg_html)
+    send_mail('Account activation', message,'admin@gmail.com',[email], html_message=msg_html, fail_silently=False)
+
+
+
+# def send_activation_code(email, activation_code):
+#     message = f'Спасибо за регистрацию, Мы вам отправили\n . Код активации: {activation_code}'
+#     send_mail(
+#         'Активация аккаунта',
+#         message,
+#         'test@gmail.com',
+#         [email]
+#     )
 
 # @app.task()
 # def send_forgot_activation_code_celery(email, activation_code):
@@ -33,4 +33,4 @@ def send_activation_code(email, activation_code):
 #         message,
 #         'test@gmail.com',
 #         [email]
-    # )
+#     )
